157b3932aee45ade1b1be710c790c92e8b24fff5
levesduzw <leventesermaul@gmail.com>
2017-11-13 23:41:18 +0100

    Indent fix

--- AT.py
+++ AT.py
@@ -4,4 +4,4 @@ if (sys.argv >= 2):
     ATcmd(argv[1])
 
 def ATcmd(_cmd):
-	subprocess.Popen('echo -e "{}\r" > /dev/ttyACM0 '.format(_cmd), shell=True) 
+    subprocess.Popen('echo -e "{}\r" > /dev/ttyACM0 '.format(_cmd), shell=True) 
