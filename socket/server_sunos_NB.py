e3f9421d07098696feac1f11bfe9cb63c8c97103
levesduzw <leventesermaul@gmail.com>
2017-12-06 02:40:56 +0100

    server NB update

--- socket/server_sunos_NB.py
+++ socket/server_sunos_NB.py
@@ -1,9 +1,9 @@
 import socket, binascii
 from datetime import datetime
 
-def hex_decode(_input):   # float -> string
-    bytess = binascii.unhexlify(_input)
-    return bytess.decode('ascii')
+def hex_decode():   # hex string -> string
+    bytess = binascii.unhexlify(_input) # returns Python 'bytes'
+    return bytess.decode('ascii')   # returns Python 'str'
 
 UDP_IP = "152.66.130.2"
 UDP_PORT = 5005
@@ -12,11 +12,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = sock.recvfrom(1024)
     print("Received message at " + datetime.now().strftime("%H:%M:%S.%f"))
-    # print("encoded: " + data.decode('ascii'))
-    print("message: " + hex_decode(data) + '\n')
-    sock.sendto(b'ok', addr)
-    
+    data = data.strip()
+    print(data + '#')
+    print(type(data))   # 'str', not 'bytes'
+    print("encoded: " + data.decode('ascii'))
+    #print("message: " + hex_decode(data) + '\n')   # not needed!
+
     with open("data.txt", 'w') as file:
-        file.write(hex_decode(data))
+        file.write(data.decode('ascii'))
